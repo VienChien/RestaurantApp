92440a644a615f4644c52a4d95e5bdff66cb425a
khanhon1704 <khanhcongtu174@gmail.com>
2023-03-26 23:15:35 +0700

    update 26/3

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,7 +7,7 @@ import 'package:flutter_application/src/resources/table_page.dart';
 import 'package:flutter_application/src/resources/pass_detail.dart';
 import 'package:flutter_application/src/resources/pass_manager.dart';
 import 'package:flutter_application/src/resources/register_page.dart';
-import 'package:flutter_application/src/resources/success.dart';
+import 'package:flutter_application/src/resources/main_support.dart';
 import 'package:flutter_application/src/resources/support.dart';
 
 class MyApp extends StatelessWidget {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SupportPage(),
+      home: mainSupport(),
     );
   }
 }
